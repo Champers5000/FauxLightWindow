056efa83303ac0a3be48fca3c16a50990ef10dad
champers5000 <champers5000@gmail.com>
2024-02-18 13:36:27 -0600

    new design based on AVR32DD20

--- LightController/inc/Application/FaultState.h
+++ LightController/inc/Application/FaultState.h
@@ -2,6 +2,13 @@
 #define FaultState_H
 
 #include <stdint.h>
+
+/**
+ * @brief Call this function from anywhere when the code detects an error. The status LED will blink out the error type and number 
+ * @param type The number of blinks the LED will flash first
+ * @param number The number of blinks the LED will flash second
+ * @note Once faultstate is entered, the MCU must be reset 
+*/
 void FaultState(uint8_t type, uint8_t number);
 
 #endif
--- LightController/inc/Application/SunDial.h
+++ LightController/inc/Application/SunDial.h
@@ -1,11 +1,33 @@
+/**
+ * ALL ANGLES IN RADIANS
+ * ALL TIME IN SECONDS FROM START OF DAY
+ * ALL DATES IN DAYS FROM START OF YEAR
+ * @ref  https://www.pveducation.org/pvcdrom/terrestrial-solar-radiation
+*/
+
 #ifndef SunDial_H
 #define SunDial_H
-
 #include <stdint.h>
+
+/**
+ * @brief Calculates the solar decliniation angle in radians for a given day of the year
+ * @param day Day of the year (1-366)
+ * @return The solar declination angle in radians
+ * @note Formula from  https://solarsena.com/solar-declination-angle-calculator/
+*/
 float solarDeclination(uint16_t day);
 
+/**
+ * @brief Calculates the error (in seconds) from normal time due to eccentricity of the earth's orbit
+ * @param day Day of the year (1-366)
+ * @return The error (in seconds) from the calculated solar time geographically (through GMT and longitude)
+*/
 float EoT(uint16_t day);
 
+/**
+ * @brief Calculates the local solar time (noon = peak sun of the day, and midnight is opposite of that)
+ * @param 
+*/
 int UTCtoSolarLocal(uint32_t UTCTime, uint16_t day, float longitude);
 
 float hourAngle(uint32_t UTCTime, uint16_t day, float longitude);
