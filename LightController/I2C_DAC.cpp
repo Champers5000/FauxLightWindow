9a0e6074efe558b2ea7dfef97a294ba39e7e3089
champers5000 <champers5000@gmail.com>
2024-07-16 08:43:59 -0500

    test dxcore

--- LightController/I2C_DAC.cpp
+++ LightController/I2C_DAC.cpp
@@ -1,8 +1,11 @@
 #include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
 #define DAC1
 #define DAC2
-#define I2C_IS_BUSY()             (internal_status.busy || !(TWI0.MSTATUS & TWI_BUSSTATE_IDLE_gc))?true:false
+#define I2C_IS_BUSY() ((!(TWI0.MSTATUS & TWI_BUSSTATE_IDLE_gc))?true:false)
 //I2C on pins PA2 and PA3
 
 uint8_t calcBaudRate(int F_SCL, float T_RISE){
@@ -15,7 +18,7 @@ void DACinit(){
   PORTA.PIN3CTRL |= PORT_PULLUPEN_bm;
 
   TWI0.CTRLA = 0;
-  TWI0.MBAUD = calcBaudRate(100000, 0.1);
+  TWI0.MBAUD = calcBaudRate(100000, 0.1f);
   TWI0.MCTRLA = 0x01;
   TWI0.MSTATUS= 0; //disable everything as we setup i2c
   TWI0.MADDR = 0; //host address is 0
@@ -26,16 +29,16 @@ void DACinit(){
   TWI0.MSTATUS |= 0b0001; //set the bus to idle for now
 }
 
-bool I2C_Write(uint8_t address, uint8_t *pData, size_t dataLength){
+bool I2C_Write(uint8_t address, uint8_t *pData, uint8_t dataLength){
   if(I2C_IS_BUSY()){
     return false;
   }
-  TWI0.MADDR = (address << 1) 
+  TWI0.MADDR = (address << 1);
   //wait for rx acknowledge and write interrupt flag
   while( !((TWI0.MSTATUS & 0b00010000 == 0) && (TWI0.MSTATUS & 0b01100000 == 0b01100000)) ){
     //when rxack = 0, WIF = 1, and CLKHOLD = 1, we are ready to send more data
   }
-  for(i=0; i<dataLength; ++i){
+  for(int i=0; i<dataLength; ++i){
     TWI0.MDATA = pData[i];
     while(TWI0.MSTATUS & 0b00010000){
       if(TWI0.MSTATUS & 0b00001000) //arblost flag
@@ -46,4 +49,5 @@ bool I2C_Write(uint8_t address, uint8_t *pData, size_t dataLength){
   }
   TWI0.MCTRLB |= 0x03; // stop bit
 
+  return false;
 }
--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -1,4 +1,4 @@
-#define F_CPU 8000000UL
+#define F_CPU 24000000UL
 #include <stdint.h>
 
 #include <avr/io.h>
@@ -9,36 +9,33 @@
 #include "inc/Application/FaultState.h"
 #include "inc/Application/XYZ1931.h"
 
-int8_t DAC0_Initialize(void)
-{
-    // DATA Register 
-	DAC0.DATA = 0x0;
-    // ENABLE enabled; OUTEN enabled; RUNSTDBY enabled; 
-	DAC0.CTRLA = 0xC1;
+#include <Wire.h>
+#include <MCP4725.h>
+int8_t DAC0_Initialize(void) {
+  // DATA Register
+  DAC0.DATA = 0x0;
+  // ENABLE enabled; OUTEN enabled; RUNSTDBY enabled;
+  DAC0.CTRLA = 0xC1;
 
-    return 0;
+  return 0;
 }
 
-void DAC0_Enable(void)
-{
-    DAC0.CTRLA |= DAC_ENABLE_bm;
+void DAC0_Enable(void) {
+  DAC0.CTRLA |= DAC_ENABLE_bm;
 }
 
-void DAC0_Disable(void)
-{
-    DAC0.CTRLA &= ~DAC_ENABLE_bm;
+void DAC0_Disable(void) {
+  DAC0.CTRLA &= ~DAC_ENABLE_bm;
 }
 
-void DAC0_SetOutput(uint16_t value)
-{
-    value     = value << DAC_DATA_gp;
-	value     = value & 0xFFC0;
-    DAC0.DATA = value;
+void DAC0_SetOutput(uint16_t value) {
+  value = value << DAC_DATA_gp;
+  value = value & 0xFFC0;
+  DAC0.DATA = value;
 }
 
-uint8_t DAC0_GetResolution(void)
-{
-    return 10;
+uint8_t DAC0_GetResolution(void) {
+  return 10;
 }
 #define StatusLED (1 << 4)  //status led on pa4
 
@@ -53,7 +50,7 @@ uint8_t DAC0_GetResolution(void)
 //   _delay_ms(1000);
 
 //   while(1){
-//     PORTA.OUT ^=StatusLED; 
+//     PORTA.OUT ^=StatusLED;
 //     _delay_ms(200);
 //   }
 
@@ -67,7 +64,7 @@ uint8_t DAC0_GetResolution(void)
 //   //     setStatusLEDDuty(i);
 //   //     _delay_ms(1);
 //   //   }
-    
+
 //   //   //test fault state
 //   //   setStatusLEDDuty(0xFFF);
 //   // }
@@ -79,43 +76,95 @@ uint8_t DAC0_GetResolution(void)
 void TCD0_init(void);
 void TCD0_enableOutputChannels(void);
 void PORT_init(void);
-void TCD0_init(void)
-{
-/* set the waveform mode */
-TCD0.CTRLB = TCD_WGMODE_DS_gc;
-/* set the signal period */
-TCD0.CMPBCLR = SIGNAL_PERIOD_EXAMPLE_VALUE;
-/* the signals are alternatively active and a small symmetric dead time is
+void TCD0_init(void) {
+  /* set the waveform mode */
+  TCD0.CTRLB = TCD_WGMODE_DS_gc;
+  /* set the signal period */
+  TCD0.CMPBCLR = SIGNAL_PERIOD_EXAMPLE_VALUE;
+  /* the signals are alternatively active and a small symmetric dead time is
 needed */
-TCD0.CMPBSET = SIGNAL_DUTY_CYCLE_EXAMPLE_VALUE + 1;
-TCD0.CMPASET = SIGNAL_DUTY_CYCLE_EXAMPLE_VALUE - 1;
-/* ensure the ENRDY bit is set */
-while(!(TCD0.STATUS & TCD_ENRDY_bm))
-{
+  TCD0.CMPBSET = SIGNAL_DUTY_CYCLE_EXAMPLE_VALUE + 1;
+  TCD0.CMPASET = SIGNAL_DUTY_CYCLE_EXAMPLE_VALUE - 1;
+  /* ensure the ENRDY bit is set */
+  while (!(TCD0.STATUS & TCD_ENRDY_bm)) {
+  }
+  TCD0.CTRLA = 0b00000001;
+}
+void TCD0_enableOutputChannels(void) {
+  /* enable write-protected register */
+  CPU_CCP = CCP_IOREG_gc;
+  TCD0.FAULTCTRL = TCD_CMPAEN_bm    /* enable channel A */
+                   | TCD_CMPBEN_bm; /* enable channel B */
+}
+void PORT_init(void) {
+  PORTA.DIR |= PIN4_bm    /* set pin 4 as output */
+               | PIN5_bm; /* set pin 5 as output */
+}
+// int main(void) {
+//   PORT_init();
+//   TCD0_enableOutputChannels();
+//   TCD0_init();
+//   /* Replace with your application code */
+//   while (1) {
+    //this is the TCD test provided by microchip
+//   }
+// }
 
+
+MCP4725 MCP(0x60);
+void setup(){
+  pinMode(PIN_PA4, OUTPUT);
+  pinMode(PIN_PD6, OUTPUT);
+  //Wire.pins(PIN_PA2,PIN_PA3);
+  Wire.swap(0);
+  //Wire.usePullups();
+  Wire.begin();
+  MCP.begin();
+  if(MCP.isConnected() == false){
+    while(true){
+      digitalWrite(PIN_PA4, HIGH);
+      delay(200);
+      digitalWrite(PIN_PA4,LOW);
+      delay(200);
+    }
+  }
+  // int pinlvl = Wire.checkPinLevels();
+  // if(pinlvl != 0x03){
+  //   while(true){
+  //     digitalWrite(PIN_PA4, HIGH);
+  //     delay(200);
+  //     digitalWrite(PIN_PA4,LOW);
+  //     delay(200*pinlvl);
+  //   }
+  // }
 }
-TCD0.CTRLA = 0b00000001;
+void writeDAC(uint16_t reg){
+  MCP.setValue(reg);
+  return;
+  uint8_t addr = (0x60 << 1);
+  uint8_t high = (reg >> 4) & 0xFF;
+  uint8_t low = ((reg & 0x0F) << 4);
+  uint8_t message[3];
+  message[0] = addr;
+  message[1] = high;
+  message[2] = low;
+  Wire.beginTransmission(addr);
+  Wire.write(0b01000000);
+  Wire.write(high);
+  Wire.write(low);
+  Wire.endTransmission();
 }
-void TCD0_enableOutputChannels(void)
-{
-/* enable write-protected register */
-CPU_CCP = CCP_IOREG_gc;
-TCD0.FAULTCTRL = TCD_CMPAEN_bm /* enable channel A */
-| TCD_CMPBEN_bm; /* enable channel B */
-}
-void PORT_init(void)
-{
-PORTA.DIR |= PIN4_bm /* set pin 4 as output */
-| PIN5_bm; /* set pin 5 as output */
-}
-int main(void)
-{
-PORT_init();
-TCD0_enableOutputChannels();
-TCD0_init();
-/* Replace with your application code */
-while (1)
-{
-;
-}
+void loop(){
+  for(int i = 0 ; i<=255; ++i){
+    analogWrite(PIN_PA4,i);
+    analogWrite(PIN_PD6,i);
+    writeDAC(i*4);
+    delay(10);
+  }
+  for(int i = 255; i>=0; --i){
+    analogWrite(PIN_PA4,i); 
+    analogWrite(PIN_PD6,i);
+    writeDAC(4*i);
+    delay(10);
+  }
 }
